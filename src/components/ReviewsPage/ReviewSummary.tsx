77edf7a5d1f0475206a44bf195f83f87655a0ead
Netanel <mordakhay.netanel@gmail.com>
2025-03-07 20:09:02 +0200

    ReviewSummary updated

--- src/components/ReviewsPage/ReviewSummary.tsx
+++ src/components/ReviewsPage/ReviewSummary.tsx
@@ -4,8 +4,10 @@ import {
   Group,
   List,
   RingProgress,
+  Stack,
   Text,
   ThemeIcon,
+  Title,
 } from "@mantine/core";
 import { IconProgressCheck, IconProgressX } from "@tabler/icons-react";
 
@@ -17,8 +19,21 @@ interface Props {
 const ReviewSummary = ({ imageURL, rating }: Props) => {
   return (
     <Box mih={300} className="text-gray">
-      <BackgroundImage src={imageURL} mih={300} radius="lg">
-        <Group align="center" justify="center" mih={300}>
+      <BackgroundImage
+        src={imageURL}
+        mih={300}
+        radius="lg"
+        style={{ overflow: "hidden" }}
+      >
+        <Group
+          align="center"
+          justify="center"
+          mih={300}
+          className="reviewSummaryBox"
+          fw={500}
+          p={20}
+          gap={30}
+        >
           {/* Rating ring */}
           <RingProgress
             size={200}
@@ -34,49 +49,55 @@ const ReviewSummary = ({ imageURL, rating }: Props) => {
           />
 
           {/* Pros */}
-          <List
-            spacing="xs"
-            size="sm"
-            center
-            icon={
-              <ThemeIcon color="green" size={24} radius="xl">
-                <IconProgressCheck size={16} />
-              </ThemeIcon>
-            }
-          >
-            <List.Item>Clone or download repository from GitHub</List.Item>
-            <List.Item>Install dependencies with yarn</List.Item>
-            <List.Item>
-              To start development server run npm start command
-            </List.Item>
-            <List.Item>
-              Run tests to make sure your changes do not break the build
-            </List.Item>
-          </List>
+          <Stack>
+            <Title>Pros</Title>
+            <List
+              spacing="xs"
+              size="sm"
+              center
+              icon={
+                <ThemeIcon color="green" size={24} radius="xl">
+                  <IconProgressCheck size={16} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>Clone or download repository from GitHub</List.Item>
+              <List.Item>Install dependencies with yarn</List.Item>
+              <List.Item>
+                To start development server run npm start command
+              </List.Item>
+              <List.Item>
+                Run tests to make sure your changes do not break the build
+              </List.Item>
+            </List>
+          </Stack>
 
           {/* Cons */}
-          <List
-            spacing="xs"
-            size="sm"
-            center
-            icon={
-              <ThemeIcon color="red" size={24} radius="xl">
-                <IconProgressX size={16} />
-              </ThemeIcon>
-            }
-          >
-            <List.Item>Clone or download repository from GitHub</List.Item>
-            <List.Item>Install dependencies with yarn</List.Item>
-            <List.Item>
-              To start development server run npm start command
-            </List.Item>
-            <List.Item>
-              Run tests to make sure your changes do not break the build
-            </List.Item>
-            <List.Item>
-              Run tests to make sure your changes do not break the build
-            </List.Item>
-          </List>
+          <Stack>
+            <Title>Cons</Title>
+            <List
+              spacing="xs"
+              size="sm"
+              center
+              icon={
+                <ThemeIcon color="red" size={24} radius="xl">
+                  <IconProgressX size={16} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>Clone or download repository from GitHub</List.Item>
+              <List.Item>Install dependencies with yarn</List.Item>
+              <List.Item>
+                To start development server run npm start command
+              </List.Item>
+              <List.Item>
+                Run tests to make sure your changes do not break the build
+              </List.Item>
+              <List.Item>
+                Run tests to make sure your changes do not break the build
+              </List.Item>
+            </List>
+          </Stack>
         </Group>
       </BackgroundImage>
     </Box>
